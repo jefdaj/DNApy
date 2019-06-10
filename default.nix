8934cece4f8e511c07de59781f1076f22c97effd
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-06-09 19:57:56 -0700

    start adding nix var substitutions

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
 with import <nixpkgs> {};
+with pythonPackages;
+
 let dnapy =
 
 { fetchgit, buildPythonPackage, wxPython, xclip, pycairo }:
@@ -6,9 +8,20 @@ let dnapy =
 buildPythonPackage {
   name = "DNApy";
   namePrefix = "";
+  # src = fetchgit {
+  #   url = "https://github.com/jefdaj/DNApy";
+  #   rev = "29355fffae9da47404e97128fd522f3e0e4561cf";
+  #   sha256 = "1w65nd9zc8dr65kp4hm7k6xapr6mx5h1fr6m3xdam9iic7mjn117";
+  # };
   src = ./.;
   pythonPath = [ wxPython xclip pycairo ];
   doCheck = false;
+  postInstall = ''
+    find $src
+    for py in $src/DNApy/*.py; do
+      substituteAllInPlace "$py"
+    done
+  '';
 }
 
 ; in callPackage dnapy {}
